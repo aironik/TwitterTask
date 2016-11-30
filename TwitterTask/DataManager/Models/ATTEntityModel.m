0dd3b2af9d226d5bc5282507a75e01870cd5d674
Oleg Lobachev <aironik@gmail.com>
2016-11-30 05:34:30 +0300

    fill entity model from JSON

--- TwitterTask/DataManager/Models/ATTEntityModel.m
+++ TwitterTask/DataManager/Models/ATTEntityModel.m
@@ -29,30 +29,71 @@
 
 + (instancetype)createFromJson:(NSDictionary *)json {
     ATTEntityModel *result = [[[self class] alloc] init];
-    [result fillFromJson:json];
+    if (![result fillFromJson:json]) {
+        return nil;
+    }
     return result;
 }
 
-- (void)fillFromJson:(NSDictionary *)json {
+- (BOOL)fillFromJson:(NSDictionary *)json {
+    BOOL result = YES;
     // TODO: обрабатывать ошибки формата и парсинга.
     NSDictionary<NSString *, NSString *> *jsonMap = self.jsonMap;
     for (NSString *key in jsonMap) {
-        NSString *keypath = jsonMap[key];
-        NSObject *value = json[key];
-        NSAssert1([self respondsToSelector:NSSelectorFromString(keypath)], @"No property '%@' in model.", keypath);
-
-        if ([value isKindOfClass:[NSString class]]) {
-            [self setValue:value forKeyPath:keypath];
-        }
-        else if ([value isKindOfClass:[NSDictionary class]]) {
-            // TODO: WRITE ME
-        }
-        else {
-            NSAssert2(NO, @"Unknown behaviour for key %@ with value class %@", key, NSStringFromClass([value class]));
-        }
+        result = (result && [self fillKeypath:jsonMap[key] withJsonValue:json[key]]);
     }
+    return result;
 }
 
+- (BOOL)fillKeypath:(NSString *)keypath withJsonValue:(NSObject *)value {
+    BOOL result = YES;
+    NSAssert1([self respondsToSelector:NSSelectorFromString(keypath)], @"No property '%@' in model.", keypath);
+    if (value == nil || value == [NSNull null]) {
+        [self setValue:nil forKeyPath:keypath];
+    }
+    else if ([value isKindOfClass:[NSString class]]) {
+        [self setValue:value forKeyPath:keypath];
+    }
+    else if ([value isKindOfClass:[NSDictionary class]]) {
+        Class propertyClass = [self getPropertyClass:keypath];
+        if (propertyClass != Nil && [propertyClass isSubclassOfClass:[ATTEntityModel class]]) {
+            ATTEntityModel *entityValue = [propertyClass createFromJson:(NSDictionary *)value];
+            [self setValue:entityValue forKeyPath:keypath];
+        }
+        else {
+            result = NO;
+        }
+    }
+    else {
+        NSAssert2(NO, @"Unknown behaviour for key (keypath %@) with value class %@", keypath, NSStringFromClass([value class]));
+        result = NO;
+    }
+    return result;
+}
+
+- (Class)getPropertyClass:(NSString *)keypath {
+    NSString *typeString = nil;
+    Class class = [self class];
+    objc_property_t property = class_getProperty(class, [keypath UTF8String]);
+    if (property != NULL) {
+        unsigned int outCount = 0;
+        objc_property_attribute_t *prop_attrs = property_copyAttributeList(property, &outCount);
+        for (int i = 0; outCount; ++i) {
+            if (prop_attrs[i].name[0] == 'T' && prop_attrs[i].value != NULL) {     // < атрибут -- тип свойства
+                typeString = [NSString stringWithUTF8String:prop_attrs[i].value];
+                break;
+            }
+        }
+        free(prop_attrs);
+    }
+    
+    if ([typeString hasPrefix:@"@"]) {      // < Только для классов ObjC
+        typeString = [typeString stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"@\""]];
+        return NSClassFromString(typeString);
+    }
+    return Nil;
+}
+    
 - (NSDictionary<NSString *, NSString *> *)jsonMap {
     if (_jsonMap == nil) {
         _jsonMap = [[[self class] createJsonMap] copy];
